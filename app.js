87ab1a252f0d7d45af7c4a3f60d73c2d17f5b436
Satya9853 <87309245+Satya9853@users.noreply.github.com>
2022-07-11 14:09:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use((req, res, next) => {
 
 app.use(challenge);
 
-app.listen(3000, () => {
-  console.log("Server.running on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
